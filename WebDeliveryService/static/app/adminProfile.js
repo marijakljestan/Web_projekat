9eb41a70e584173e239af60f495475e9c2236802
AnicaDjukic <djukic.ra20.2018@uns.ac.rs>
2021-07-06 22:55:05 +0200

    Odradjena izmena profila admina

--- WebDeliveryService/static/app/adminProfile.js
+++ WebDeliveryService/static/app/adminProfile.js
@@ -1,8 +1,7 @@
-Vue.component("manager-profile", {
+Vue.component("admin-profile-page", {
 	data: function () {
 		    return {
-			  user: null,
-			  restaurant: '',
+		      user: null,
 		      errorMessage: ''
 		    }
 	},
@@ -26,17 +25,14 @@ Vue.component("manager-profile", {
           </button>
         </div>
         <div class="collapse navbar-collapse" id="myNavbar">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#/manager"><span class="glyphicon glyphicon-home"></span> Početna</a></li>
-            <li><a href="#/managerProfile"><span class="glyphicon glyphicon-user"></span> Moj Profil</a></li>
-            <li><a v-on:click="showRestaurant"><span class="glyphicon glyphicon-tasks"></span> Moj restoran</a></li>
-            <li><a href="#/ordersManager"><span class="glyphicon glyphicon-cutlery"></span> Porudžbine</a></li>
-            <li><a href="#/customersManger"><span class="glyphicon glyphicon-globe"></span> Kupci</a></li>
-            <li><a href="#/commentsManager"><span class="glyphicon glyphicon-comment"></span> Komentari</a></li>
-      
+            <ul class="nav navbar-nav">
+            <li class="active"><a href="#/admin"><span class="glyphicon glyphicon-home"></span> Početna</a></li>
+            <li><a href="#/adminProfile"><span class="glyphicon glyphicon-user"></span> Moj Profil</a></li>
+            <li><a href="#/userProfilesView"><span class="glyphicon glyphicon-folder-open"></span> Korisnici</a></li>
+            <li><a href="#/commentsAdmin"><span class="glyphicon glyphicon-comment"></span> Komentari</a></li>
           </ul>
           <ul class="nav navbar-nav navbar-right">
-            <li><a href="#/"><span class="glyphicon glyphicon-log-out"></span> Odjavite se</a></li>
+            <li> <a href="#/"><span class="glyphicon glyphicon-log-out"></span> Odjavite se</a> </li>
           </ul>
         </div>
       </div>
@@ -50,7 +46,7 @@ Vue.component("manager-profile", {
         
             <div class="col-lg-6" style="margin-left: 120px; margin-top: 150px;">
             
-            	<label style="color: rgb(30, 31, 104);">Ime:</label><br/>
+                <label style="color: rgb(30, 31, 104);">Ime:</label><br/>
                 <input type="text" v-model="user.name" class="input-fields"><br/><br/>
                 <label style="color: rgb(30, 31, 104);">Prezime:</label><br/>
                 <input type="text" v-model="user.surname" class="input-fields"><br/><br/>
@@ -69,36 +65,24 @@ Vue.component("manager-profile", {
               	</select><br/><br/> 
                 <label style="color: rgb(30, 31, 104);">Lozinka:</label><br/>
                 <input type="text" v-model="user.password" class="input-fields"><br/><br/> 
-            </div>                
+            </div>             
         </div>  
         <p style="color:red; position: absolute; top: 430px; left: 630px;">{{errorMessage}}</p>
         <button v-on:click="acceptChanges" class="edit-profile" style="position: absolute; top: 450px; left: 630px; width: 250px;">Izmeni podatke</button>
     </div>
-  
+
       <footer class="container-fluid text-center">
         <p>Online Food Delivery Copyright</p>  
       </footer>
-   </div>
+  </div>
 `
-	,
+	, 
 	mounted () {
-        axios
+     axios
           .get('/user/')
           .then(response => (this.user = response.data))
-          
-        axios
-	      .get('/manager/')
-	      .then(response => (this.restaurant = response.data.restaurant))
-    }, 
+    },
 	methods : {
-		showRestaurant : function() {
-			axios
-	          .get('/manager/')
-	          .then(response => {
-		    		window.location.href = "#/restaurantManager?id="+ response.data.restaurant;
-		      })
-		},
-				
 		acceptChanges : function (event) {
 				
 				event.preventDefault();
@@ -129,28 +113,10 @@ Vue.component("manager-profile", {
 	    				surname : this.user.surname,
 	    				gender : this.user.gender,
 	    				dateOfBirth : d,
-	    				role : 'MANAGER'				
-    				}
-    				let newManager = {
-    					username : this.user.username,
-						password : this.user.password,
-	    				name : this.user.name,
-	    				surname : this.user.surname,
-	    				gender : this.user.gender,
-	    				dateOfBirth : d,
-	    				role : 'MANAGER',
-	    				restaurant: this.restaurant
+	    				role : 'ADMIN'				
     				}
 					axios 
 		    			.put('/users/edit', JSON.stringify(newUser))
-		    			.then(response => {
-								location.reload();
-		    			})
-		    			.catch(error => {
-						    console.log(error.response)
-						});
-					axios
-						.put('/manager/edit', JSON.stringify(newManager))
 		    			.then(response => {
 								location.reload();
 		    			})
--- WebDeliveryService/static/app/app.js
+++ WebDeliveryService/static/app/app.js
@@ -3,7 +3,7 @@ const LoginPage = { template: '<login-page></login-page>' }
 const RestaurantPage = { template: '<restaurant-page></restaurant-page>' }
 const AdministratorPage = { template: '<administrator-page></administrator-page>' }
 const AddNewRestaurant = { template: '<addNewRestaurant-page></addNewRestaurant-page>' }
-const UserProfile = { template: '<user-profile-page></user-profile-page>' }
+const AdminProfile = { template: '<admin-profile-page></admin-profile-page>' }
 const UserProfilesView = {template: '<user-profiles-page></user-profiles-page>'}
 const CommentsViewAdmin = {template: '<comments-admin></comments-admin>'}
 const ManagerPage = { template: '<manager-page></manager-page>' }
@@ -28,7 +28,7 @@ const router = new VueRouter({
 	    { path: '/admin', component: AdministratorPage },
 	    { path: '/restaurant', component: RestaurantPage },
 	    { path: '/addNewRestaurant', component: AddNewRestaurant },
-	    { path: '/userProfile', component: UserProfile },
+	    { path: '/adminProfile', component: AdminProfile },
 	    { path: '/userProfilesView', component: UserProfilesView },
 	    { path: '/commentsAdmin', component: CommentsViewAdmin },
 	    { path: '/managerProfile', component: ManagerProfile },
